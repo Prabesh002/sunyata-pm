36a8dcd97d00393f7fa1a8bcd6acd126ff098774
Nellow <prabeshpathak002@gmail.com>
2024-12-28 10:19:11 +0545

    some normal

--- src/app/features/products/product-detail/product-detail.component.ts
+++ src/app/features/products/product-detail/product-detail.component.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { RouterLink, ActivatedRoute, Router } from '@angular/router';
 import { ProductsService } from '../products.service';
 import { Product } from '../models/product.model';
+import { Category } from '../../../shared/enums/category.enum';
+import { Brand } from '../../../shared/enums/brand.enum';
 
 @Component({
   selector: 'app-product-detail',
@@ -12,6 +14,9 @@ import { Product } from '../models/product.model';
 })
 export class ProductDetailComponent implements OnInit {
   product?: Product;
+    categoryName: string | null = null;
+    brandName: string | null = null;
+
 
   constructor(
     private productsService: ProductsService,
@@ -31,18 +36,22 @@ export class ProductDetailComponent implements OnInit {
   loadProduct(id: number): void {
     this.productsService.getProduct(id).subscribe({
       next: (product) => {
-        if (product) {
-          this.product = product;
-        } else {
-          this.router.navigate(['/products']);
+          if (product) {
+            console.log(product);
+            this.product = product;
+              this.categoryName = product.categoryId !== null ? Category[product.categoryId] : 'N/A';
+                this.brandName = product.brandId !== null ? Brand[product.brandId] : 'N/A';
+         } else {
+            this.router.navigate(['/products']);
         }
       },
       error: (error) => {
-        console.error('Error loading product:', error);
-        this.router.navigate(['/products']);
+          console.error('Error loading product:', error);
+          this.router.navigate(['/products']);
       }
     });
   }
+
 
   goBack(): void {
     this.router.navigate(['/products']);
---
+++ src/app/shared/enums/brand.enum.ts
@@ -0,0 +1,9 @@
+//FOR TESTING PURPOSES
+
+export enum Brand {
+    Apple = 1,
+    Samsung = 2,
+    Nike = 3,
+    Adidas = 4,
+    Sony = 5
+}
